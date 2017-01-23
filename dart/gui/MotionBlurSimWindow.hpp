9d018fe166c69610c037d1e64e55c000bc568576
hxbloom <hxbloom@gmail.com>
2017-01-23 13:31:37 -0500

    motion blur with no apparent bugs

--- dart/gui/MotionBlurSimWindow.hpp
+++ dart/gui/MotionBlurSimWindow.hpp
@@ -2,13 +2,51 @@
 //  MotionBlurSimWindow.hpp
 //  dart
 //
-//  Created by 徐东 on 1/22/17.
+//  Created by Dong Xu on 1/22/17.
 //
 //
 
-#ifndef MotionBlurSimWindow_hpp
-#define MotionBlurSimWindow_hpp
+#ifndef DART_GUI_MOTIONBLURSIMWINDOW_HPP_
+#define DART_GUI_MOTIONBLURSIMWINDOW_HPP_
 
-#include <stdio.h>
+#include <vector>
+#include <Eigen/Dense>
 
-#endif /* MotionBlurSimWindow_hpp */
+#include "dart/gui/SimWindow.hpp"
+
+namespace dart {
+namespace gui {
+
+class MotionBlurSimWindow : public SimWindow
+{
+public:
+
+  /// \brief
+  MotionBlurSimWindow();
+
+  /// \brief
+  virtual ~MotionBlurSimWindow();
+    
+  // Set the frequency of motion blur
+  // Default is 1 (record position of every frame)
+  void setMotionBlurFrequency(int _val);
+    
+  // Override the render function in dart/gui/Win3D.hpp
+  // To draw the motion image
+  // Render function is called once per GUI display time
+  // but in MotionBlurSimWindow, draw function will run in motion blur frequency
+  void render() override;
+
+  // Override the display timer,
+  // Move the part of "step" in world function to the render function
+  void displayTimer(int _val) override;
+    
+protected:
+  int mMotionBlurFrequency;
+
+}; // End of Class Definition
+ 
+} // namespace gui
+} // namespace dart
+
+#endif // DART_GUI_MOTIONBLURSIMWINDOW_HPP_
